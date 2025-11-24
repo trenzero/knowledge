ad10c35fc59c26fdab602c87059ffedb86c6b4c7
TRenZero <107801158+trenzero@users.noreply.github.com>
2025-11-25 05:31:53 +0800

    创建 sw.js

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'knowledge-base-v1';
+const urlsToCache = [
+    '/',
+    '/style.css',
+    '/app.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
